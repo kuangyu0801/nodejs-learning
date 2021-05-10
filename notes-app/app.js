5ac427a2604f2a033a9d008e7f7ba2f312be87a8
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-05-10 13:33:20 +0200

    Finish 3.12

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,5 @@
 //const add = require('./util.js')
+const chalk = require('chalk');
 
 //console.log(add(1, 5))
 
@@ -7,4 +8,4 @@ const getNotes = require('./nodes')
 
 console.log(getNotes())
 console.log('isEmail: ' + validator.isEmail('li@example.com'))
-console.log('isUrl: ' + validator.isEmail('li@example.com'))
+console.log(chalk.green('isUrl: ' + validator.isEmail('li@example.com')))
