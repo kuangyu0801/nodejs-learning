d8b679ae1335cad147add78b9c45d1ed392ec025
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-05-16 23:20:19 +0200

    Finish lec-37

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('postman-request') 
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 /*
 const url = 'http://api.weatherstack.com/current?access_key=808b55fed3f288bf11c87b4cc8850d79&query=34.0544,-118.2439'
 console.log('Sending request: ' + url)
@@ -35,7 +36,23 @@ request( {url: gUrl, json: true}, (error, response) => {
 })
 */
 
-geocode('New York', (error, data) => {
+/* geocode('New York', (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 })
+ */
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+//forecast(34.0544, -118.2439, (error, data) => {
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('postman-request') 
+
+const forecast = (longitude, latitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=808b55fed3f288bf11c87b4cc8850d79&query=' + latitude + ',' + longitude
+    request({url: url, json: true}, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined)                
+        } else if (response.body.error) {
+            callback('Unable to find location', url)
+        } else {
+            const data = response.body.current
+            callback(undefined, data.weather_descriptions[0])
+            callback(undefined, 'It is currently ' + data.temperature + '. It feels like ' + data.feelslike + ' out')
+            callback(undefined, 'There is a ' + data['precip'] + ' % probability of rain')
+        }
+    })
+}
+
+module.exports = forecast
